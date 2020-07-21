e9cb387958bc882b5bbc1677645bb88ad9a015f5
Fahimur Reza <fahimur@Fahimurs-MBP.lan>
2020-07-20 19:33:54 -0700

    code refactored

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,29 +3,29 @@ import './styles.css'
 import { ReactComponent as CheckMark } from './Asset/checkmark02.svg';
 import { ReactComponent as DeleteIcon } from './Asset/delete01.svg';
 
-const TodoItem = ({item, index, isCompleted, toggleIsCompleted, deleteItem}) => {
+const TodoItem = ({ item, index, isCompleted, toggleIsCompleted, deleteItem }) => {
 
   const [showDelete, setShowDelete] = React.useState(null)
   const handleClick = () => toggleIsCompleted(index)
   const removeItem = () => deleteItem(index)
-    
-  return(
-    <div 
+
+  return (
+    <div
       className='item'
-      onMouseOver={() => setShowDelete(true) }
+      onMouseOver={() => setShowDelete(true)}
       onMouseLeave={() => setShowDelete(false)}
     >
       <div className='round' onClick={handleClick}>
         {isCompleted && <span> <CheckMark className='markIcon' /> </span>}
-      </div> 
+      </div>
       <span className={`itemContent ${isCompleted ? "completed" : ""}`} >{item}</span>
-      { showDelete && 
-        <span> 
-          <DeleteIcon 
-            className='deleteIcon' 
-            onClick = {removeItem}
-          /> 
-        </span> 
+      {showDelete &&
+        <span>
+          <DeleteIcon
+            className='deleteIcon'
+            onClick={removeItem}
+          />
+        </span>
       }
     </div>
   )
